90e5f6289b5841605fb2074836f8786119edbdad
Snapster-amb <jcperschon@gmail.com>
2020-01-07 14:09:24 -0700

    Created primitive kafka service with asynchronous publish method.

--- src/services/KafkaService.ts
+++ src/services/KafkaService.ts
@@ -1,3 +1,27 @@
-export class KafkaServie {
-  constructor () {}
+import {Kafka} from "kafkajs";
+const uuid1 = require('uuid/v1');
+
+export class KafkaService {
+  private kafka: Kafka;
+
+  public constructor(brokers: string[]) {
+    this.kafka = new Kafka({
+      clientId: uuid1(),
+      brokers: brokers
+    });
+  }
+
+  public async publish(topic: string, key: string, value: string) {
+    const producer = this.kafka.producer();
+    await producer.connect();
+    await producer.send({
+      topic: topic,
+      messages: [{
+        key: key,
+        value: value,
+      }]
+    });
+    await producer.disconnect();
+    // Add store update here
+  }
 }
