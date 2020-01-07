08979984df155ca8b340a3c6d4e9560a7f31b6a5
James Schmidt <james.schmidt@wework.com>
2020-01-07 11:12:07 -0700

    Add sidebar stub

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import Button from '@material-ui/core/Button';
 import './App.css';
+import Sidebar from './sidebar';
 
 const App: React.FC = () => {
   return (
@@ -21,6 +22,7 @@ const App: React.FC = () => {
           Learn React
         </a>
       </header>
+      <Sidebar />
     </div>
   );
 }
--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -1,10 +1,54 @@
 import * as React from 'react';
+import { Fab } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 class Sidebar extends React.Component<any, any>  {
   public render() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+
+    const handleClose = () => {
+      setOpen(false);
+    };
+
     return (
       <div>
-        Sidebar
+        Server
+        <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Connect to Server</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="server"
+            label="Server"
+            type="text"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Connect
+          </Button>
+        </DialogActions>
+      </Dialog>
       </div>
     );
   }
