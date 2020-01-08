7a7dbd22011122cd6d42edefba04815507393a6f
Caleb Van Rhee <caleb.vanrhee@teem.com>
2020-01-08 10:05:41 -0700

    Adding dialog

--- src/editor/produce/produce.tsx
+++ src/editor/produce/produce.tsx
@@ -4,12 +4,20 @@ import Button from '@material-ui/core/Button';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
 import './produce.css';
 
 export default function Produce() {
 
   const [value, setValue] = React.useState('json');
+  const [open, successAction] = React.useState(false);
   const [textValue, setTextValue] = React.useState()
+
+  // Should be moved to a service that can start dialog with success and failure
+  function Alert(props: AlertProps) {
+    return <MuiAlert elevation={2} variant="filled" {...props} />;
+  }
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue((event.target as HTMLInputElement).value);
@@ -18,6 +26,19 @@ export default function Produce() {
   const handleTextAreaChange = (event: React.ChangeEvent) => {
     setTextValue((event.target as HTMLTextAreaElement).value);
   }
+
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    successAction(false);
+  }
+
+  const handleClick = () => {
+    // TODO send data to Kafka and success or fail dialog
+    successAction(true);
+  };
 
   return (
     <div className="content">
@@ -29,7 +50,13 @@ export default function Produce() {
           </RadioGroup>
       </FormControl>
       <textarea className="text-editor" value={textValue} onChange={handleTextAreaChange}/>
-     <Button>Submit</Button>
+     <Button onClick={handleClick}>Submit</Button>
+     <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+        <Alert onClose={handleClose} color="success">
+          Successfully sent data
+        </Alert>
+      </Snackbar>
+     
     </div>
   );
 }
