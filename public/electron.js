e050d804593f71fbca559f6d23a36521231b7dd5
Will Smith <undertakingyou@gmail.com>
2020-01-08 15:38:50 -0700

    revert out Will's weird environment changes

--- public/electron.js
+++ public/electron.js
@@ -9,7 +9,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({ width: 1800, height: 1200 });
     mainWindow.loadURL(
         isDev
-            ? "http://williams-mbp:3000"
+            ? "http://localhost:3000"
             : `file://${path.join(__dirname, "../build/index.html")}`
     );
     mainWindow.on("closed", () => (mainWindow = null));
