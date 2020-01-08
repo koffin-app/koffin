94940df2996e9c28b55bcef5ae49ed885868a182
Caleb Van Rhee <caleb.vanrhee@teem.com>
2020-01-08 10:45:24 -0700

    Updating css for css

--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import BrokerModal from './brokerModal';
+import './sidebar.css'
 
 export default function Sidebar() {
   const [open, setOpen] = React.useState(false);
@@ -18,10 +19,13 @@ export default function Sidebar() {
 
   return (
     <div className="sidebar">
-      Servers
-      <Fab size="small" color="primary" aria-label="add" onClick={handleClickOpen}>
-        <AddIcon />
-      </Fab>
+      <header className="header">
+        <div>Servers</div>
+        <Fab size="small" color="default" aria-label="add" onClick={handleClickOpen}>
+          <AddIcon />
+        </Fab>  
+      </header>
+      
       <BrokerModal selectedValue={selectedValue} open={open} onClose={handleClose} />
     </div>
   );
